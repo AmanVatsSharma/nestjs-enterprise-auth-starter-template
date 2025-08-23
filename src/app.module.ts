5599e37a29765c76ccda90b8330e08c50688f0be
AmanVatsSharma <aman95026@gmail.com>
2025-08-24 03:47:10 +0530

    throttler module added in main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 100,
+    }])
   ],
   controllers: [AppController],
   providers: [AppService],
