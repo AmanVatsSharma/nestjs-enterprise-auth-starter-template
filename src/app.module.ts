3405534075ac4ed9cc51c7357e5bc9e2ecbfb0b0
AmanVatsSharma <aman95026@gmail.com>
2025-08-24 03:55:30 +0530

    typeorm init in main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -12,7 +13,18 @@ import { ThrottlerModule } from '@nestjs/throttler';
     ThrottlerModule.forRoot([{
       ttl: 60,
       limit: 100,
-    }])
+    }]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // set to false in production
+      ssl: process.env.DB_SSLMODE === 'require' ? { rejectUnauthorized: false } : false,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
