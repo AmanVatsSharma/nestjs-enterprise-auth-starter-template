04e6a3df8d5ec6d218390c60598b7e9026a09039
AmanVatsSharma <aman95026@gmail.com>
2025-08-24 03:39:11 +0530

    added  global pipes & global throttler guard

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
+  // app.useGlobalGuards(app.get(ThrottlerGuard));
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
