9e8cf9f3e2080fb23fff3e54757cd9342df6ddcc
Aman Sharma <aman95026@gmail.com>
2025-09-08 03:41:57 +0000

    chore: Added Related Dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,15 +18,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     }]),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST ?? 'localhost',
       port: parseInt(process.env.DB_PORT ?? '5432'),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME ?? 'nest',
+      password: process.env.DB_PASSWORD ?? 'secret',
+      database: process.env.DB_NAME ?? 'nestdb',
       autoLoadEntities: true,
       synchronize: true, // set to false in production
       ssl: process.env.DB_SSLMODE === 'require' ? { rejectUnauthorized: false } : false,
     }),
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,26 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to BharatERP API – see /docs!';
+  }
+
+  // getHtmlPage(): react.ReactNode{
+  //   return <> 
+  //   <div>
+  //   hello worold
+  //   </div>
+  //   </>
+  // }
+
+  getHealth() {
+    // This Function check sfor the health
+
+    const healthStatus = {
+      department: 'Health',
+      message: "Ok",
+      uptime: process.uptime().toLocaleString()
+    }
+
+    return healthStatus
   }
 }
